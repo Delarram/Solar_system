cd1e818735f45fe77b8275679a31ead85e4d212f
Delarram <tunyanaing960@gmail.com>
2022-12-22 22:27:33 +0630

    added forminputfield

--- lib/screens/detailpage/detail_page.dart
+++ lib/screens/detailpage/detail_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_universe/data.dart';
-import 'constants.dart';
+
+import '../../data/data.dart';
+import '../../resources/constants.dart';
+
 
 class DetailPage extends StatelessWidget {
   final PlanetInfo planetInfo;
