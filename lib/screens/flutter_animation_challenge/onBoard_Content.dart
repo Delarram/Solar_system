fddd5de040bbecb5bcecb5f6008184b81376d58c
Delarram <tunyanaing960@gmail.com>
2023-04-25 16:49:28 +0630

    landing content

---
+++ lib/screens/flutter_animation_challenge/onBoard_Content.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnBoardContent extends StatefulWidget {
+  const OnBoardContent({Key key}) : super(key: key);
+
+  @override
+  State<OnBoardContent> createState() => _OnBoardContentState();
+}
+
+class _OnBoardContentState extends State<OnBoardContent> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
