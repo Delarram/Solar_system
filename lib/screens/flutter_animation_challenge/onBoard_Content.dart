95e1972a8e4b84e7666b4e61bf4fd6957d7c474b
Delarram <tunyanaing960@gmail.com>
2023-04-25 23:48:55 +0630

    complete animationchallenge

--- lib/screens/flutter_animation_challenge/onBoard_Content.dart
+++ lib/screens/flutter_animation_challenge/onBoard_Content.dart
@@ -1,15 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_universe/screens/flutter_animation_challenge/Landing_content.dart';
+import 'package:flutter_universe/screens/flutter_animation_challenge/signup_form.dart';
 
-class OnBoardContent extends StatefulWidget {
-  const OnBoardContent({Key key}) : super(key: key);
+
+class OnboardContent extends StatefulWidget {
+
 
   @override
-  State<OnBoardContent> createState() => _OnBoardContentState();
+  State<OnboardContent> createState() => _OnboardContentState();
 }
 
-class _OnBoardContentState extends State<OnBoardContent> {
+class _OnboardContentState extends State<OnboardContent> {
+   PageController _pageController;
+  // double _progress;
+  @override
+  void initState() {
+    _pageController = PageController()
+      ..addListener(() {
+        setState(() {});
+      });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final double progress =
+    _pageController.hasClients ? (_pageController.page ?? 0) : 0;
+
+    return SizedBox(
+      height: 400 + progress * 140,
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Column(
+            children: [
+              const SizedBox(height: 16),
+              Expanded(
+                child: PageView(
+                  controller: _pageController,
+                  children: const [
+                    LandingContent(),
+                    SignUpForm()
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            height: 56,
+            bottom: 32 + progress * 140,
+            right: 16,
+            child: GestureDetector(
+              onTap: () {
+                if (_pageController.page == 0) {
+                  _pageController.animateToPage(1,
+                      duration: const Duration(milliseconds: 400),
+                      curve: Curves.ease);
+                }
+              },
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomLeft,
+                    end: Alignment.topRight,
+                    stops: [0.4, 0.8],
+                    colors: [
+                      Color.fromARGB(255, 239, 104, 80),
+                      Color.fromARGB(255, 139, 33, 146)
+                    ],
+                  ),
+                ),
+                child: DefaultTextStyle(
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 92 + progress * 32,
+                        child: Stack(
+                          fit: StackFit.passthrough,
+                          children: [
+                            Opacity(
+                              opacity: 1 - progress,
+                              child: const Text("Get Started"),
+                            ),
+                            Opacity(
+                              opacity: progress,
+                              child: const Text(
+                                "Create account",
+                                maxLines: 1,
+                                overflow: TextOverflow.fade,
+                                softWrap: false,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Icon(
+                        Icons.chevron_right,
+                        size: 24,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
